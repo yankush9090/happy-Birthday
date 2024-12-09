9bb4c3c87850b2f468553134f4f121a4450d1e1f
yankush9090 <YANKUSH9090@GMAIL.COM>
2024-12-09 20:07:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!',' Rasgullah' ],
+			strings: [ 'idiot', 'Number 1 !',' rasgullah' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
