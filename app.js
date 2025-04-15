fc495104271d87f650ea909558edd76be1b23a64
yankush9090 <YANKUSH9090@GMAIL.COM>
2025-04-15 23:22:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!',' Mnavi' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!',' Manvi' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
